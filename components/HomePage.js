7b37fcdd139b5fd4c4badd0280b3b99ce16940ea
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2024-02-19 18:51:23 -0500

    minor refactor

--- components/HomePage.js
+++ components/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
         <div className=" w-full grid grid-cols-1 grid-rows-3 gap-6">
             <div className=" w-full h-[40%]"><Hero /></div>
             <div className=" w-full h-[40%]"><Accomplishments /></div>
-            <div className=" w-full h-[40%]"><OurServices /></div>
+            {/*<div className=" w-full h-[40%]"><OurServices /></div> */}
         </div>
     )
 }
